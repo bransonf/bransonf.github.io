221dafaadf905a4e40070190aa55c2d8da9ebf0f
bransonf <branson.fox@slu.edu>
2020-08-14 19:49:08 -0500

    add css art example

---
+++ art/fullscreen.js
@@ -0,0 +1,32 @@
+
+// Open Method
+function openFullscreen() {
+    var elem = document.body
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
+
+// Close Method
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+    }
+}  
+
+document.body.onclick = () => {
+    openFullscreen();
+    closeFullscreen();
+};
